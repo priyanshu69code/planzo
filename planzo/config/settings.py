8036c5532aa34f91e49a34c414b9d6461b970543
Priyanshu Kumar <99123745+priyanshu69code@users.noreply.github.com>
2025-04-14 12:50:42 +0530

    Set DEBUG to False in settings for production readiness

--- planzo/config/settings.py
+++ planzo/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = False
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 print(ALLOWED_HOSTS)
 # ALLOWED_HOSTS = ["planzo","planzo.com","localhost",]
