08139d622c64ae3a87302ad200aeb549297546c1
Priyanshu Kumar <99123745+priyanshu69code@users.noreply.github.com>
2025-04-17 10:06:00 +0530

    Update DEBUG setting to use environment variable for production configuration

--- planzo/config/settings.py
+++ planzo/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 print(ALLOWED_HOSTS)
 # ALLOWED_HOSTS = ["planzo","planzo.com","localhost",]
